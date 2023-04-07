8904abaf58bf490043ac3b38526e10642f9ebd53
sajeel <sajeelahmed21@gmail.com>
2023-04-07 07:17:10 +0200

    base_proyecto

---
+++ functions/config.php
@@ -0,0 +1,11 @@
+<?php
+    /** SITE URLS */
+    const SITE_ROOT                 = 'https://localhost/biblioteca/';
+    const IMGS_ROOT                 = SITE_ROOT."assets/images/";
+    const CSS_ROOT                  = SITE_ROOT."assets/css/";
+    const JS_ROOT                   = SITE_ROOT."assets/js/";
+    const BOOTSTRAP_ROOT            = SITE_ROOT."libs/bootstrap/";
+    const LIBS_ROOT                 = SITE_ROOT."libs/";
+    /** YT API */
+
+?>
